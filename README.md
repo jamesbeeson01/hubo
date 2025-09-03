166a9baec120016dcb95a4c2f80bb4f03a42d5a8
James Beeson <jamesbeeson01@gmail.com>
2025-09-03 11:05:13 -0600

    Drawer buttons work

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# hubo
-Personal hub home for connecting personalized apps and features
+# Hubo
+
+Personal hub home for connecting personalized desktop apps and features
+
+## TODO
+
+- [ ] Place drawers in the correct positions
+    - [ ] Hover: show shadow 1
+    - [ ] Click shadow 1: 
+        - [ ] Small drawer appears over top
+        - [ ] Shadow disappears
+    - [ ] Small drawer appears when click shadow
+- [ ] Give drawers animations
+- [ ] Fix window sizing and spacing
+- [ ] Implement apps structure
+    - [ ] Dynamic adding/sizing
+- [ ] Make the apps searchable
+- [ ] Implement search UI
+    - [ ] Have it add or remove elements based on number characters in input
+- [ ] Implement search function
+- [ ] Connect input to AI chat (show in backend terminal)
+- [ ] Create chat UI (loren ipsum on enter)
+- [ ] Build first app
--- hubo-app/src/render.js
+++ hubo-app/src/render.js
@@ -1,16 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
     const closebtn = document.getElementById('close-btn');
-    if(closebtn) {
-        closebtn.addEventListener('click', () => {
-            // fade out
-            const body = document.getElementsByTagName('body')[0];
-            body.style.transition = 'opacity 0.15s';
-            body.style.opacity = '0';
-            
-            // close window
-            body.addEventListener('transitionend', () => {
-                window.preload.closeWindow();
-            }, { once: true });
-        });
-    }
+    const smallshadow = document.getElementById('small-drawer-shadow-btn');
+    const smalldrawer = document.getElementById('small-drawer');
+    const allshadow = document.getElementById('all-apps-shadow-btn');
+    const allapps = document.getElementById('all-apps');
+
+    closebtn.addEventListener('click', () => {
+        // fade out
+        const body = document.getElementsByTagName('body')[0];
+        body.style.transition = 'opacity 0.15s';
+        body.style.opacity = '0';
+        
+        // close window
+        body.addEventListener('transitionend', () => {
+            window.preload.closeWindow();
+        }, { once: true });
+    });
+
+    smallshadow.addEventListener('click', () => {
+        smalldrawer.style.display = 'block';
+        smallshadow.style.display = 'none';
+        allshadow.style.display = 'block';
+    });
+
+    allshadow.addEventListener('click', () => {
+        allshadow.style.display = 'none';
+        allapps.style.display = 'block';
+        allapps.style.position = 'fixed';
+        smallshadow.style.display = 'block';
+        smalldrawer.style.display = 'none';
+    });
+
 });
